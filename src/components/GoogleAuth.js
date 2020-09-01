cca67b3da28fb3ea81884159d26b22818df77e53
Ademola Sobaki <demolasobaki@gmail.com>
2020-09-01 15:20:32 +0100

    get and update auth state from google oauth20 apis

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -1,17 +1,43 @@
 import React, { Component } from 'react'
 
 class GoogleAuth extends Component {
+	state = {
+		isSignedIn: null,
+	}
+
 	componentDidMount() {
 		window.gapi.load('client:auth2', () => {
-			window.gapi.client.init({
-				clientId: process.env.REACT_APP_GOOGLE_OAUTH_CLIENT_ID,
-				scope: 'email',
-			})
+			window.gapi.client
+				.init({
+					clientId: process.env.REACT_APP_GOOGLE_OAUTH_CLIENT_ID,
+					scope: 'email',
+				})
+				.then(() => {
+					this.auth = window.gapi.auth2.getAuthInstance()
+					this.setState({ isSignedIn: this.auth.isSignedIn.get() })
+					this.auth.isSignedIn.listen(this.onAuthChange)
+				})
 		})
 	}
 
+	onAuthChange = () => {
+		this.setState({
+			isSignedIn: this.auth.isSignedIn.get(),
+		})
+	}
+
+	renderAuthButton() {
+		if (this.state.isSignedIn === null) {
+			return <div>Not sure</div>
+		} else if (this.state.isSignedIn) {
+			return <div>I am Signed in</div>
+		} else {
+			return <div>I am not signed In</div>
+		}
+	}
+
 	render() {
-		return <div>GoogleAuth</div>
+		return <div>{this.renderAuthButton()}</div>
 	}
 }
 
